fcb54c42bdcf5357ac3814e38b4ea959c0e03741
Sanjeevnathan <127003228@sastra.ac.in>
2025-08-08 10:03:26 +0530

    Update DemonKnight.js
    path error

--- DemonKnight.js
+++ DemonKnight.js
@@ -6,10 +6,11 @@ function resizeCanvas() {
 }
 resizeCanvas();
 window.addEventListener('resize', resizeCanvas);
-let  isGame=false;
+let  isGame=true;
+let  isPause=false;
 let score=0;
 let HI=0;
-const enemies=[]
+let enemies=[];
 const bufferedKeysList = ['z', 'x', 'c','ArrowUp','d'];
 let keys = {};
 let enemyAnimations = {};
@@ -17,7 +18,12 @@ let bufferedKeys = {}; // Timers for buffered keys
 // Listen for the 'keydown' event to detect when a key is pressed
 document.addEventListener('keydown', function(event) {
   const key = event.key;
-  keys[event.key] = true;  // Store the key as pressed
+  keys[event.key] = true;// Store the key as pressed
+
+  if (key == 'Escape') {
+    isPause = !isPause;
+  }
+
   if (bufferedKeys[key]) {
     clearTimeout(bufferedKeys[key]);
     delete bufferedKeys[key];
@@ -104,15 +110,17 @@ class Player extends Character{
   }
   update(deltaTime){
     super.update(deltaTime);
-    if(!this.dead){
+    if(!this.dead && !isPause){
       this.movements();
-    }else{
+    }
+    else{
       if(this.frameIndex>=this.animations.death.frames-1){
         isGame=false;
       }
     }
   }
   movements(){
+
     if(!keys['Control']){
       this.isCrouch=false;
     }
@@ -136,7 +144,6 @@ class Player extends Character{
       }
     }
     if((!keys['ArrowUp'] && !this.isGrounded) || this.jumpCount==this.maxJumps ){
-      this.setAnimation('fall');
       if(this.y<this.defaultY){
         this.y+=this.gravity*1.5;
       }
@@ -175,11 +182,11 @@ const deathImg=new Image();
 const hurtImg=new Image();
 const attackImg=new Image();
 const idleImg=new Image(); 
-flyImg.src='/Demon-Knight/Demon/Sprites/with_outline/FLYING.PNG';
-deathImg.src='/Demon-Knight/Demon/Sprites/with_outline/DEATH.PNG';
-hurtImg.src='/Demon-Knight/Demon/Sprites/with_outline/HURT.PNG';
-idleImg.src='/Demon-Knight/Demon/Sprites/with_outline/IDLE.PNG';
-attackImg.src='/Demon-Knight/Demon/Sprites/with_outline/ATTACK.PNG';
+flyImg.src='./Demon/Sprites/with_outline/FLYING.png';
+deathImg.src='./Demon/Sprites/with_outline/DEATH.png';
+hurtImg.src='./Demon/Sprites/with_outline/HURT.png';
+idleImg.src='./Demon/Sprites/with_outline/IDLE.png';
+attackImg.src='./Demon/Sprites/with_outline/ATTACK.png';
 flyImg.onload = () => {
   enemyAnimations = {
     flying: { image: flyImg, frames: 4 },
@@ -192,8 +199,8 @@ flyImg.onload = () => {
 
 class Enemy extends Character{
       
-  constructor(x,y,animations,frameWidth,frameHeight,scale=1){
-    super(x,y,animations,frameWidth,frameHeight,scale);this.currentAnim='flying';this.health=100;
+  constructor(x,y,health,animations,frameWidth,frameHeight,scale=1){
+    super(x,y,animations,frameWidth,frameHeight,scale);this.currentAnim='flying';this.maxHealth=health;
     this.angle=0;
     this.speed=4;
     this.frq=0.02;
@@ -202,11 +209,11 @@ class Enemy extends Character{
     this.verticalChaseRange=canvas.height;
     this.attackRange=50;
     this.isChasing=false;
-    this.maxHealth=100;
+    this.health=this.maxHealth;
   }
   update(deltaTime,player){
     super.update(deltaTime);
-    if(!this.dead){
+    if(!this.dead && !isPause){
       this.chase(player);
     }else{
       if(this.frameIndex>=this.animations.death.frames-1){
@@ -216,6 +223,7 @@ class Enemy extends Character{
   }
 
   chase(player){
+    if(isPause){return;}
     const dx=Math.abs(this.x-player.x);const dy=Math.abs(this.y-player.y);
     if(dx<this.chaseRange && dy<this.verticalChaseRange){
       this.isChasing=true;
@@ -236,8 +244,8 @@ class Enemy extends Character{
   }
 }
 
-function spawnEnemy(){
-  const enemy=new Enemy(canvas.width*Math.random(),canvas.height*0.3,enemyAnimations,81,71,2);
+function spawnEnemy(health){
+  const enemy=new Enemy(canvas.width*Math.random(),canvas.height*0.3,health,enemyAnimations,81,71,2);
   enemies.push(enemy);
 }
 
@@ -248,7 +256,7 @@ function collide(a,b){
   const dx = centre1.x - centre2.x;
   const dy = centre1.y - centre2.y;
   const distanceSquared = dx * dx + dy * dy;
-  if(a.isCrouch && a.isGrounded &&(!keys['x'] && !keys['c']) && (dx>2||dy>=2||dy<2)){
+  if(a.isCrouch && a.isGrounded &&(!keys['x'] && !keys['c']) && (dx>2 && Math.abs(dy)>2)){
     return false;
   }
       
@@ -257,62 +265,55 @@ function collide(a,b){
 
   return distanceSquared <= combinedRadiusSquared;
 }
- 
+
 const waves = {
-  wave1: { no: 1, enemyCount: 5, isFinished: false, next: 'wave2' },
-  wave2: { no: 2, enemyCount: Math.pow(5, 2), isFinished: false, next: 'wave3' },
-  wave3: { no: 3, enemyCount: Math.pow(5, 3), isFinished: false, next: 'wave4' },
-  wave4: { no: 4, enemyCount: Math.pow(5, 4), isFinished: false, next: null },
+  wave0:{ no: 0, enemyCount: 0, isFinished: false, next: 'wave1',eHealth:300 ,damage:200},
+  wave1: { no: 1, enemyCount: 5, isFinished: false, next: 'wave2',eHealth:300 ,damage:200},
+  wave2: { no: 2, enemyCount: Math.pow(3, 2), isFinished: false, next: 'wave3',eHealth:800, damage:200 },
+  wave3: { no: 3, enemyCount: Math.pow(3, 3), isFinished: false, next: 'wave4', eHealth:1300 ,damage:300},
+  wave4: { no: 4, enemyCount: Math.pow(3, 4), isFinished: false, next: 'wave5' , eHealth:2000, damage:500},
+  wave5: { no: 5, enemyCount: Math.pow(3, 5), isFinished: false, next: 'wave6',eHealth:2300 ,damage:500},
+  wave6: { no: 6, enemyCount: Math.pow(3, 6), isFinished: false, next: 'wave7',eHealth:2800, damage:800 },
+  wave7: { no: 7, enemyCount: Math.pow(3, 7), isFinished: false, next: 'wave8', eHealth:3300 ,damage:800},
+  wave8: { no: 8, enemyCount: Math.pow(3, 8), isFinished: false, next: 'wave9' , eHealth:4000, damage:1000},
+  wave9: { no: 9, enemyCount: Math.pow(3, 9), isFinished: false, next: 'wave10',eHealth:4300 ,damage:1000},
+  wave10: { no: 10, enemyCount: Math.pow(3, 10), isFinished: false, next: 'wave11',eHealth:4800, damage:1100 },
+  wave11: { no: 11, enemyCount: Math.pow(3, 11), isFinished: false, next: 'wave12', eHealth:5300 ,damage:1200},
+  wave12: { no: 12, enemyCount: Math.pow(3, 12), isFinished: false, next: null , eHealth:6000, damage:1500},
 };
 
-let curWave='wave1';
+let curWave='wave0';
 let waveAnnounced = false;
 let announcementText = '';
 let announcementTimer = 0;
 
  // Load images for each animation
 const walkImage = new Image();
-walkImage.src = '/Demon-Knight/FreeKnight_v1/Colour1/Outline/120x80_PNGSheets/_Run.PNG';
+walkImage.src = './FreeKnight_v1/Colour1/Outline/120x80_pngSheets/_Run.png';
 const idleImage = new Image();
-idleImage.src = '/Demon-Knight/FreeKnight_v1/Colour1/Outline/120x80_PNGSheets/_Idle.PNG';
+idleImage.src = './FreeKnight_v1/Colour1/Outline/120x80_pngSheets/_Idle.png';
 const slideImage=new Image();
-slideImage.src='/Demon-Knight/FreeKnight_v1/Colour1/Outline/120x80_PNGSheets/_SlideFull.PNG';
+slideImage.src='./FreeKnight_v1/Colour1/Outline/120x80_pngSheets/_SlideFull.png';
 const attackImage=new Image();
-attackImage.src='/Demon-Knight/FreeKnight_v1/Colour1/Outline/120x80_PNGSheets/_Attack.PNG';
+attackImage.src='./FreeKnight_v1/Colour1/Outline/120x80_pngSheets/_Attack.png';
 const attack2Image=new Image();
-attack2Image.src='/Demon-Knight/FreeKnight_v1/Colour1/Outline/120x80_PNGSheets/_Attack2.PNG';
+attack2Image.src='./FreeKnight_v1/Colour1/Outline/120x80_pngSheets/_Attack2.png';
 const crouchAttackImage=new Image();
-crouchAttackImage.src='/Demon-Knight/FreeKnight_v1/Colour1/Outline/120x80_PNGSheets/_CrouchAttack.PNG';
+crouchAttackImage.src='./FreeKnight_v1/Colour1/Outline/120x80_pngSheets/_CrouchAttack.png';
 const crouchImage=new Image();
-crouchImage.src='/Demon-Knight/FreeKnight_v1/Colour1/Outline/120x80_PNGSheets/_Crouch.PNG';
+crouchImage.src='./FreeKnight_v1/Colour1/Outline/120x80_pngSheets/_Crouch.png';
 const comboImage=new Image();
-comboImage.src='/Demon-Knight/FreeKnight_v1/Colour1/Outline/120x80_PNGSheets/_AttackCombo2hit.PNG';
+comboImage.src='./FreeKnight_v1/Colour1/Outline/120x80_pngSheets/_AttackCombo2hit.png';
 const dashImage=new Image();
-dashImage.src='/Demon-Knight/FreeKnight_v1/Colour1/Outline/120x80_PNGSheets/_Dash.PNG';
+dashImage.src='./FreeKnight_v1/Colour1/Outline/120x80_pngSheets/_Dash.png';
 const deathImage=new Image();
-deathImage.src='/Demon-Knight/FreeKnight_v1/Colour1/Outline/120x80_PNGSheets/_Death.PNG';
+deathImage.src='./FreeKnight_v1/Colour1/Outline/120x80_pngSheets/_Death.png';
 const hitImage=new Image();
-hitImage.src='/Demon-Knight/FreeKnight_v1/Colour1/Outline/120x80_PNGSheets/_Hit.PNG';
+hitImage.src='./FreeKnight_v1/Colour1/Outline/120x80_pngSheets/_Hit.png';
 const jumpImage=new Image();
-jumpImage.src='/Demon-Knight/FreeKnight_v1/Colour1/Outline/120x80_PNGSheets/_Jump.PNG';
+jumpImage.src='./FreeKnight_v1/Colour1/Outline/120x80_pngSheets/_Jump.png';
 const fallImage=new Image();
-fallImage.src='/Demon-Knight/FreeKnight_v1/Colour1/Outline/120x80_PNGSheets/_JumpFallInBetween.PNG';
-
-let imagesLoaded = 0;
-const totalImages = 18; 
-
-// This function will be called whenever an image finishes loading
-function imageLoaded() {
-  imagesLoaded++;
-  if (imagesLoaded === totalImages) {
-    isGame=true;
-  }
-}
-const images = [walkImage, idleImage, slideImage, attackImage, attack2Image, crouchAttackImage, crouchImage, comboImage, dashImage, deathImage, hitImage, jumpImage, fallImage, flyImg, deathImg, hurtImg, idleImg, attackImg];
-
-images.forEach(image => {
-  image.onload = imageLoaded;
-});
+fallImage.src='./FreeKnight_v1/Colour1/Outline/120x80_pngSheets/_JumpFallInBetween.png';
 
 walkImage.onload = () => {
   idleImage.onload = () => {
@@ -332,12 +333,78 @@ walkImage.onload = () => {
       fall:{image: fallImage, frames: 2}
     };
 
+    class PreLoader{
+      constructor(src,onComplete){
+        this.video=document.createElement('video');
+        this.video.src=src;
+        this.video.muted=true;
+        this.video.preload='auto';
+        this.video.playsInline=true;
+        this.state='intro';
+        this.onComplete=onComplete;
+
+        this.video.addEventListener('canplay',()=>{
+          this.video.play();
+          this.drawloop();
+        });
+
+        this.video.addEventListener('timeupdate',()=>{
+          if(this.state=='intro' && this.video.currentTime>=3){
+            this.video.pause();
+            this.state='wait';
+          }
+  
+          if(this.state=='outro' && this.video.currentTime>=5){
+            this.state='done';
+            this.video.pause();
+            this.onComplete();
+          }
+        });
+        document.addEventListener('keydown', (e) => {
+          if (e.key == 'Enter' && this.state == 'wait') {
+            this.state = 'outro';
+            this.video.currentTime = 3;
+            this.video.play();
+          }
+        });
+
+      }
+
+      drawloop(){
+        if(this.state=='done')return;
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        ctx.drawImage(this.video,0,0,canvas.width,canvas.height);
+        requestAnimationFrame(() => this.drawloop());
+      }
+    }
+
+ 
     const ply1 = new Player(canvas.width/2,canvas.height*0.7, animations, 120, 80,2); 
-                      
+    let waveKills=0;
+    let lastTime = 0;
+
+    const preloader=new PreLoader('preloader.mp4',()=>{requestAnimationFrame(gameLoop);});
+
     // Animation loop
     function gameLoop(timestamp) {
       const deltaTime = timestamp - lastTime;
       lastTime = timestamp;
+
+      if(isPause){
+        ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        ctx.font = "60px Arial";
+        ctx.fillStyle = "white";
+        ctx.textAlign = "center";
+        ctx.fillText("PAUSED", canvas.width / 2, canvas.height / 2);
+
+        ctx.font = "30px Arial";
+        ctx.fillText("Press 'ESC' to Resume", canvas.width / 2, canvas.height / 2 + 50);
+        requestAnimationFrame(gameLoop);
+        return;
+      }
+
       if(isGame){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.font = "30px Arial";
@@ -365,9 +432,9 @@ walkImage.onload = () => {
 
           if (distanceX < distanceThreshold && distanceY < distanceThreshold) {
             if (collide(ply1, enemy) && ply1.isAttack || ply1.currentAnim=='crouchAttack') {
-              enemy.health -= Math.floor(200/16);score+=Math.floor(100/16);
+              enemy.health -= Math.floor(waves[curWave].damage/16);score+=Math.floor(waves[curWave].damage/32);
             }else if(collide(ply1,enemy) && (ply1.currentAnim!='dash')){
-              ply1.health-=10;ply1.setAnimation('hit');
+              ply1.health-=100/16;
             }
           }
           enemy.update(deltaTime,ply1);
@@ -376,6 +443,8 @@ walkImage.onload = () => {
           if (enemy.health <= 0 && !enemy.dead) {
             enemy.setAnimation('death');
             enemy.dead = true;
+            waveKills++;
+            console.log(`Wave kills= ${waveKills}`);
           }
 
           if(!enemy.dead){
@@ -399,24 +468,35 @@ walkImage.onload = () => {
 
         }
         }else{
-        ctx.font = "50px Arial";
-        ctx.fillStyle = "red";
-        ctx.textAlign = "center";
-        ctx.fillText("Game Over", canvas.width / 2, canvas.height / 2);
-        if(score>HI){
-          HI=score;
-        }
-        score=0;
-        if(keys['r']){
-          isGame = true;
-          ply1.dead = false;  
-          ply1.health = 10000; // Reset health
-          ply1.x = canvas.width / 2; // Reset position
-          curWave='wave1';//Reset Wave
-          // Reset the enemies array
-          enemies.splice(0, enemies.length);
-        }
-
+          ctx.font = "50px Arial";
+          ctx.fillStyle = "red";
+          ctx.textAlign = "center";
+          if(curWave!='wave12'){ctx.fillText("Game Over", canvas.width / 2, canvas.height / 2);}
+          else{
+            ctx.fillStyle = "green";
+            ctx.textAlign = "center";
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.fillText("Press R to Restart", canvas.width / 2, canvas.height / 2);
+          }
+          if(score>HI){
+            HI=score;
+          }
+          score=0;
+          if(keys['r']){
+            console.clear();
+            isGame = true;
+            ply1.dead = false;
+            waveKills=0;  
+            waveAnnounced=false;
+            for(let i=0;i<12;i++){
+              waves[`wave${i}`].isFinished=false;
+            }
+            ply1.health = 10000; // Reset health
+            ply1.x = canvas.width / 2; // Reset position
+            curWave='wave0';//Reset Wave
+            // Reset the enemies array
+            enemies.splice(0, enemies.length);
+          }
       }
           
       if (waveAnnounced && isGame) {
@@ -426,31 +506,49 @@ walkImage.onload = () => {
         ctx.fillText(announcementText, canvas.width / 2, canvas.height / 2);
         announcementTimer--;
         if (announcementTimer <= 0) {
+          if(announcementText=='Victory' && isGame){
+           isGame=false;
+          }
           waveAnnounced = false;
           announcementText = '';
+        
         }
       }
-
       requestAnimationFrame(gameLoop);
     }
 
-    let lastTime = 0;
-    requestAnimationFrame(gameLoop);
+    
     setInterval(() => {
-      const currentWave = waves[curWave];
-      if (enemies.length < Math.pow(5, waves[curWave].no)) {
-        spawnEnemy();
+
+      if(isPause && !isGame) {
+        return;
       }
 
-      if (!currentWave.isFinished && enemies.length >= currentWave.enemyCount) {
+      const currentWave = waves[curWave];
+      if (enemies.length < Math.pow(5, waves[curWave].no) && !isPause) {
+        spawnEnemy(waves[curWave].eHealth);
+      }
+
+      if (!currentWave.isFinished && waveKills>=waves[curWave].enemyCount) {
         currentWave.isFinished = true;
         if (currentWave.next) {
           curWave = currentWave.next;
-
+          waveKills=0;
+          //Heal
+          if(waves[curWave].no %2==0){
+            ply1.health+=4000;
+            if(ply1.health>ply1.maxHealth) ply1.health=ply1.maxHealth;
+            console.log("Healed "+4000+"HP");
+          }
+          console.log("Damage:"+waves[curWave].damage);
           // Show wave announcement
           waveAnnounced = true;
           announcementText = `Wave ${waves[curWave].no}`;
           announcementTimer = 120; 
+        }else{
+          waveAnnounced = true;
+          announcementText = 'Victory';
+          announcementTimer = 1000;
         }
       }
     }, 2000);   
